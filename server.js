ccec7ea47ad04d33314a0c9e16129b26e4e9fc6d
Sachinjha08 <sachinjha08@gmail.com>
2024-09-30 20:51:26 +0530

    blogger

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const connectDB = require("./config/connectDB");
+connectDB();
+const morgan = require("morgan");
+const cors = require("cors");
+const userRoute = require("./routes/userRoutes");
+const blogRoute = require("./routes/blogRoutes");
+const cookie = require("cookie-parser");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(express.json());
+app.use(cookie());
+
+const PORT = process.env.PORT || 7000;
+
+// app.use("/", (req, res) => {
+//   res.status(201).send({
+//     message: `App is running on ${PORT}`,
+//   });
+// });
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/blog", blogRoute);
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
