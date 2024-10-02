a854229f070fe2a6c99efc5f0f8464f4a4d04ea1
Sachinjha08 <139585222+Sachinjha08@users.noreply.github.com>
2024-10-02 12:22:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(morgan("dev"));
 app.use(
   cors({
     credentials: true,
-    origin: "https://blogger-frontend-nyay.onrender.com",
+    origin: "https://blogger-due9.onrender.com",
   })
 );
 
